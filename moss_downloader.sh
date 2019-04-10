408daf0bc48276ae8c4fe8492aaba0ac9a64ca0a
vamsi <vmsbdpd@gmail.com>
2019-04-11 01:39:03 +0530

    Initial Commit

---
+++ moss_downloader.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# saner programming env: these switches turn some bugs into errors
+set -o errexit -o pipefail -o noclobber -o nounset
+
+! getopt --test > /dev/null 
+if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
+    echo 'I’m sorry, `getopt --test` failed in this environment.'
+    exit 1
+fi
